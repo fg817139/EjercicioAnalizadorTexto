cfc762aa007970fe13f56cbf33ea0d957e6fedac
Jesus Hincapie <jahlon@gmail.com>
2023-05-30 10:36:25 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='EjercicioAnalizadorTexto',
+    version='1.0.0',
+    packages=['tests', 'analizador', 'analizador.modelo'],
+    url='',
+    license='MIT license',
+    author='jehincapie',
+    author_email='jahlon@gmail.com',
+    description='Exercise for assessing object-oriented programming skills in python'
+)
